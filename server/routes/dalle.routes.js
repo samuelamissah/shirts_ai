50bd64bd3d68ad6ab3068755b60f600932372de5
samuelamissah <skamissah@ug.edu.gh>
2024-01-16 22:31:17 +0000

    jsj

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI({  apiKey: process.env.OPENAI_API_KEY });
 
-const openai = new OpenAIApi(config);
-
-router.route('/').get((req, res) => {
+router.route('/').get((_, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
 })
 
@@ -20,14 +16,19 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const response = await openai.completions.create({
+      model: 'text-davinci-003', // Update the model parameter to the new model name
       prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
+      max_tokens: 0,
+      temperature: 0,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: '\n'
     });
 
-    const image = response.data.data[0].b64_json;
+    const image = response.data.choices[0].text;
 
     res.status(200).json({ photo: image });
   } catch (error) {
